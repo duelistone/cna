ceb5cefa35ff0e60c6829b7404e6d9528784b1ee
Luis Ares <duelistone@hotmail.com>
2018-12-31 22:53:10 -0500

    Fixed bug in repertoire visitor affecting opening test.

--- rep_visitor.py
+++ rep_visitor.py
@@ -11,9 +11,9 @@ visited_hashes = set()
 def rep_visitor(board, player=None):
     '''Visits repertoire nodes that are children of given board in dfs.
     Returns board/move pairs for player.'''
+    # In case of originally given node being repeated later on in search tree
+    # For other nodes, this is redundant due to loop below
     hashValue = chess.polyglot.zobrist_hash(board)
-    if hashValue in visited_hashes:
-        raise StopIteration
     visited_hashes.add(hashValue)
     subrep = G.rep.ww
     if player == chess.WHITE and board.turn == chess.BLACK:
@@ -23,9 +23,13 @@ def rep_visitor(board, player=None):
     elif player == chess.BLACK and board.turn == chess.BLACK:
         subrep = G.rep.bb
     for entry in subrep.find_all(board):
-        if board.turn == player or player == None:
-            yield board, entry.move()
-        board.push(entry.move())
-        yield from rep_visitor(board, player)
-        board.pop()
+        child_board = board.copy()
+        child_board.push(entry.move())
+        child_hash = chess.polyglot.zobrist_hash(child_board)
+        if child_hash not in visited_hashes:
+            if (board.turn == player or player == None):
+                yield board, entry.move()
+            board.push(entry.move())
+            yield from rep_visitor(board, player)
+            board.pop()
 
